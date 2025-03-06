df97b0bd0c1d3857ebe6f864265eb43f418fdbb8
TharHtet236401 <tharhteta3@gmail.com>
2025-03-06 18:13:29 +0700

    single article read

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,10 @@ router.get('/', (req, res) => {
   res.render('index', {title: 'Product Listing', articles: articles});
 });
 
+router.get('/articles/:id', (req, res) => {
+  const article = articles.find(article => article.id === parseInt(req.params.id));
+  res.render('article', {title: article.name, article: article});
+});
+
 export default router;
 
