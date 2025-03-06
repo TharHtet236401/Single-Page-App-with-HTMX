3a7ad170421fddbe668a17a3cafbffa82fdb3e8e
TharHtet236401 <tharhteta3@gmail.com>
2025-03-06 22:55:53 +0700

    Implement live search functionality for articles using HTMX

--- routes/index.js
+++ routes/index.js
@@ -33,5 +33,12 @@ router.get('/contact', (req, res) => {
   res.render('contact', {title: 'Contact Us'});
 });
 
+
+router.post('/articles/search', (req, res) => {
+  const {search} = req.body;
+  const results = articles.filter(article => article.name.toLowerCase().includes(search.toLowerCase()));
+  res.render('partials/list', {articles: results});
+});
+
 export default router;
 
